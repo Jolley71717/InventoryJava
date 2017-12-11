03c85b3fbc917c8251f1b2a4f8198ad860ef33f9
Luke Dutton <duttonluke03@gmail.com>
2017-12-10 23:39:47 -0700

    added some notes for where to put sqlite piece and where to put the phant reset piece

--- InventoryAccess/src/java/com/jolley/Main.java
+++ InventoryAccess/src/java/com/jolley/Main.java
@@ -3,8 +3,6 @@ package com.jolley;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.tools.javac.jvm.Items;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -15,44 +13,44 @@ public class Main {
 
     public static void main(String[] args)  {
         String stringURL = "http://localhost:8080/output/YYVAOe8Qr8F4MreoQVYWUaqK6AA.json";
-            String jsonData ="";
+        String jsonData ="";
+
         try{
              jsonData = readUrl(stringURL);
         }catch (Exception e){
             System.out.println(e);
         }
 
+        //If it's null, you shouldn't be printing stuff out
         if (jsonData != ""){
             List<Item> items = getItems(jsonData);
             for (Item f : items){
                 System.out.println(f.description + " at time " + f.getTimestamp());
+
+                //This is where the sqlite piece takes place
+
+
+                //After successful insert, remove the information from phant
             }
+
+
         }
-
-
     }
 
     private static List<Item> getItems(String jsonInfo){
         ObjectMapper mapper = new ObjectMapper();
-
         try{
+                mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY,true);
 
-            //System.out.println(jsonData);
+                List<Item> items = mapper.readValue(jsonInfo,new TypeReference<List<Item>>(){});
+                Collections.sort(items, new ItemComparator());
 
-            mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY,true);
+                return items;
 
-            List<Item> items = mapper.readValue(jsonInfo,new TypeReference<List<Item>>(){});
-            Collections.sort(items, new ItemComparator());
-
-
-            //System.out.println(items);
-            return items;
         } catch (Exception e){
             System.out.println(e);
         }
-
         return null;
-
     }
 
     private static String readUrl(String urlString) throws Exception {
